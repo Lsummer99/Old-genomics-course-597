ee2e7b9f1cb7cab68320d2ccec601b38f3fbe2a6
Lsummer99 <lsmason@umass.edu>
2020-08-31 15:36:15 +0000

    ..

---
+++ lab1_lauren.R
@@ -0,0 +1,26 @@
+##exercise_1
+##for x=3 and y=15, compute the sum, difference, product, and quotient of x and y
+x <- 3
+y <- 15
+x + y
+x - y
+x * y
+x / y
+
+##exercise_2
+##for x=2 and y=3, Test x^5 is greater than y^4 and return a logical value
+x <- 2; y <- 3
+x^5 > y^4
+
+##exercise_3
+##create a vector of the values 22,62,148,43 and 129.
+##determine the sum of the vector.
+##divide each value in the vector by the sum to determine related frequency
+x <- c(22,62,148,43,129)
+sum (x)
+x / 404
+
+##exercise_4
+##create a vector of the nucleotides A,T,C and G. Sort the vector.
+nucleotides <- c("A","T","C","G")
+sort(nucleotides)
